e51449de58549c6bc58205b29101522e05d89556
shirshakmaskey <shirshak.maskey17@gmail.com>
2020-02-02 21:38:42 -0800

    basic blockchain

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -1,2 +1,26 @@
+import java.util.Date;
+
 public class Block {
+    public int index;
+    public long timestamp;
+    public String data;
+    public String prev_hash;
+    public String hash;
+
+    public Block(int index, String data, String prev_hash) {
+        this.index = index;
+        this.timestamp = new Date().getTime();
+        this.data = data;
+        this.prev_hash = prev_hash;
+        this.hash = getHash();
+    }
+
+    private String getHash() {
+        return Encryption.sha256(this.index + this.timestamp + this.data + this.prev_hash);
+    }
+
+    public String toString() {
+        return ("Block #" + this.index + "\n\tmined at: " + this.timestamp + "\n\tData: " + this.data + "\n\tHash: {"
+                + this.hash + "}\n");
+    }
 }
--- src/main/java/Blockchain.java
+++ src/main/java/Blockchain.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
 public class Blockchain {
+    public static ArrayList<Block> blockchain = new ArrayList<>(); // The blockchain is implemented as an arraylist of Blocks
+    public static ArrayList<String> transactions = new ArrayList<>();
+
+    public final static int NUM_BLOCKS = 20;
+
+
+    public static Block createGenesisBlock() {
+        return new Block(0, "\"Everything starts from here!\"", "0");
+    }
+
+    public static Block createNextBlock(Block prevBlock) {
+        return new Block(prevBlock.index + 1, transactions.toString(), prevBlock.hash);
+    }
+
+    public static void printChain() {
+        for (int i = 0; i < blockchain.size(); i++) {
+            System.out.println(blockchain.get(i));
+        }
+    }
+    public static void main(String args[]) {
+        blockchain.add(createGenesisBlock()); // create genesis block and add it to the chain
+        // add 20 more blocks to the chain
+        for (int i = 0; i < NUM_BLOCKS; i++) {
+            Block next = createNextBlock(blockchain.get(i));
+            blockchain.add(next);
+        }
+        printChain();
+    }
+
 }
